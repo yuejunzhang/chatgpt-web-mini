4ec63b6ca04f0a3c60db19464d838b16655bfbe2
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2023-08-01 11:03:09 +0800

    Update popupMenu.js

--- scripts/popupMenu.js
+++ scripts/popupMenu.js
@@ -3,12 +3,57 @@
     var parentElement = document.getElementById('chatlog');
     var appendMssageText="";
     var selectedText="";
- 
+    var Menu_X,Menu_Y;
+
+
+    // 添加selectionchange事件监听器
+    // document.addEventListener('selectionchange', handleSelectionChange);
+    // //  // 获取文本选择改变时的回调函数
+    //  function handleSelectionChange(event) {
+    //     selectedText = window.getSelection().toString();
+    //         if(selectedText==""){
+    //             hideCustomMenu();
+    //             return;}
+    //         var target =   window.getSelection().focusNode;
+    //         // 检查目标元素是否是父元素的后代
+    //         const isDescendant = parentElement.contains(target);
+    //         if (isDescendant) {
+    //             getCursorPosition(target)
+    //             // showCustomMenu(target.changedTouches[0].pageX, target.changedTouches[0].pageY+10);
+    //             showCustomMenu(Menu_X, Menu_Y);
+    //             appendMssageText=target.textContent;
+    //         } else {
+    //             hideCustomMenu();
+    //         }
+    //   }
   
+ 
+
+ 
+
+    //   function getCursorPosition(target) {
+    //     // const cursorPositionTextElement = document.getElementById('cursorPositionText');
+    //     const selection = window.getSelection();
+    //     const range = document.createRange();
+    //     if (selection.rangeCount > 0) {
+    //         range.setStart(target, selection.focusOffset);
+    //         range.collapse(true);
+    //         const rect = range.getClientRects()[0];
+    //         Menu_X = rect.left;
+    //         Menu_Y = rect.top;
+    //         // 以下是光标的位置坐标
+    //         console.log('光标位置 X:', Menu_X);
+    //         console.log('光标位置 Y:', Menu_Y);
+    //     } else {
+    //         console.log('没有获取到光标位置。');
+    //     }
+    // }
+     //////////////////////////////////////////////////////////////////////////////   
         
     if(isMobile){
         // 监听文本选择事件
         document.addEventListener('touchend', function (event) {
+            event.preventDefault();
             selectedText=getSelectedText() ;
             var target = event.target;
             // 检查目标元素是否是父元素的后代
@@ -21,6 +66,18 @@
                 hideCustomMenu();
             }
         });
+
+        const menuUpEvent = (event) => {
+            // ev.preventDefault();
+            var target = event.target;
+            if (target == document.querySelector("#menu_appendAsk")){askForExplanation();}
+            if (target == document.querySelector("#menu_copy")){copyText();}
+            if (target == document.querySelector("menu_serch")){searchOnBaidu();}
+        }
+
+        customMenu.ontouchend = menuUpEvent;
+
+
     } 
     else{    
         // 监听文本选择事件
@@ -75,7 +132,6 @@
     
         // 自定义菜单选项 - 复制文本
         function copyText() {
-                alert(selectedText);
             //  selectedText = getSelectedText();
             // 创建一个隐藏的textarea元素
             var textarea = document.createElement('textarea');
