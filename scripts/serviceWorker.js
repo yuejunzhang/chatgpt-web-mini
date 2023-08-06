050786ef17835e181e44644fe05daa22c0b5b701
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2023-08-06 17:28:34 +0800

    Update serviceWorker.js add cluade-2

--- scripts/serviceWorker.js
+++ scripts/serviceWorker.js
@@ -21,4 +21,4 @@ if (isMobile) {
     link.href = "https://cdn.jsdelivr.net/gh/timruffles/mobile-drag-drop@3.0.0-rc.0/release/default.css";
     document.body.appendChild(link);
 }
-let envAPIEndpoint, envAPIKey;
+let envAPIEndpoint, envAPIKey,envCluadeAPIEndpoint, envCluadeAAPIKey ;
