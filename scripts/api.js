90857a2d602e7d99003a670b9ba295529e8583a8
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2023-08-06 08:30:59 +0800

    Update api.js

--- scripts/api.js
+++ scripts/api.js
@@ -2234,6 +2234,8 @@ const autoSpeechEvent = (content, ele, force = false, end = false) => {
         ele.classList.add("showVoiceCls");
         ele.querySelector('.optionItems').lastChild.className = "optionItem pauseVoice";
     }
+        //跳过##
+    content=content.replace(/#/g, '');
     if (existVoice >= 2) {
         voiceContentQuene.push(content);
         voiceEndFlagQuene.push(end);
