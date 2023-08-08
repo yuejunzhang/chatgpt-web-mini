ba1ff67cddd80e4e5ee8b35b64fad4b1f7790116
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2023-08-08 11:44:49 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
 envAPIEndpoint="https://api.xyhelper.cn/"
 envAPIKey="sk-api-xyhelper-cn-free-token-for-everyone-xyhelper"
-envClaudeAPIEndpoint="https://claude2chatgpt-zhangyuejun.up.railway.app"
+envClaudeAPIEndpoint="https://claude2chatgpt-zhangyuejun.up.railway.app/"
 envClaudeAAPIKey=""
